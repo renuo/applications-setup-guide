62f8ebd9ffcabaae85e75fb5fcca5acab2ef2680
Cyril Kyburz <cyril.kyburz@renuo.ch>
2016-08-31 14:22:53 +0200

    add support folder to be required

--- templates/spec/spec_helper.rb
+++ templates/spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'simplecov'
 SimpleCov.start 'rails'
 SimpleCov.minimum_coverage 100
 
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
