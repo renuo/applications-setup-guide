a958a44073b2a506509380d5724186b7f24c8166
Alessandro Rodi <coorasse@gmail.com>
2018-08-27 17:17:19 +0200

    Update rails_helper.rb

--- templates/spec/rails_helper.rb
+++ templates/spec/rails_helper.rb
@@ -13,6 +13,7 @@ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 
   config.before(:each, type: :system) do
