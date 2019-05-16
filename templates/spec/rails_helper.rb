0482d7991dd32cfaaf162635899255700758b02c
Simon Huber <simon.huber@renuo.ch>
2019-05-16 15:07:49 +0200

    Hush Puma in RSpec

--- templates/spec/rails_helper.rb
+++ templates/spec/rails_helper.rb
@@ -20,6 +20,10 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
 
+  config.before(:all, type: :system) do
+    Capybara.server = :puma, { Silent: true }
+  end
+
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
