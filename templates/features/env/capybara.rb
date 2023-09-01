867a316989b395d0fedd31a16e6f7753325f1acd
schmijos <schmijos@users.noreply.github.com>
2023-09-01 13:26:30 +0000

    deploy: 3233b701a8973909fc0502b2665cfae6c7a531be

--- templates/features/env/capybara.rb
+++ templates/features/env/capybara.rb
@@ -1,20 +1,2 @@
-Capybara.register_driver :selenium_chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-
-  %w[
-    incognito
-    disable-extensions
-    auto-open-devtools-for-tabs
-    window-size=1280,800
-  ].each { options.add_argument _1 }
-
-  %w[
-    headless
-    disable-gpu
-  ].each { options.add_argument _1 } if %w[1 on true].include?(ENV['HEADLESS'])
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
-
 Capybara.default_driver = :rack_test
 Capybara.javascript_driver = :selenium_chrome_headless
