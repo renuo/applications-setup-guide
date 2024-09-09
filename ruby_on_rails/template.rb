e7e9e8e27d3682d83e045d24133c5c58433965eb
coorasse <coorasse@users.noreply.github.com>
2024-09-09 09:16:06 +0000

    deploy: 5554d8f2b2f8d81c4c42b261b8be9064c6c343f1

--- ruby_on_rails/template.rb
+++ ruby_on_rails/template.rb
@@ -2,16 +2,19 @@
 
 gsub_file "Gemfile", /gem "rubocop-rails-omakase"/, "gem \"renuocop\""
 
+# add "ruby file: ".ruby-version" to the Gemfile under the line starting with "source"
+insert_into_file "Gemfile", after: /^source.*\n/ do
+  <<~RUBY
+    ruby file: ".ruby-version"
+  RUBY
+end
+
 # override the content of .rubocop.yml
 create_file ".rubocop.yml", force: true do
   <<~RUBOCOP
     inherit_gem:
       renuocop: config/base.yml
   RUBOCOP
-end
-
-after_bundle do
-  run "bundle exec rubocop -A"
 end
 
 create_file "bin/fastcheck", force: true do
@@ -30,3 +33,15 @@ create_file "bin/fastcheck", force: true do
 end
 
 run "chmod +x bin/fastcheck"
+
+after_bundle do
+  run "bundle exec rubocop -A"
+
+  puts "We want to be sure that you are aware of what has been done by the Renuo template."
+  puts "Please read the following list of changes and confirm that you understand them."
+  ask "The Gemfile ruby version has been set to the version in the .ruby-version file. You know what this means. Ok?"
+  ask "Your project is now using renuocop instead of rubocop-rails-omakase. You know both gems and why it has been replaced. Ok?"
+  ask "A .rubocop.yml file has been created with the default configuration. You know what this means. Ok?"
+  ask "A bin/fastcheck script has been created to run rubocop with the default configuration."
+  ask "You can run the script with `bin/fastcheck`. You know why we have a bin/fastcheck file. Ok?"
+end
