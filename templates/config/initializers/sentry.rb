a445ce9e23f95399fb4083f7cb57a3d03dd960c3
Simon Isler <simon-isler@gmx.ch>
2025-12-01 11:31:51 +0100

    Update sentry config

--- templates/config/initializers/sentry.rb
+++ templates/config/initializers/sentry.rb
@@ -6,7 +6,19 @@ if defined? Sentry
   Sentry.init do |config|
     filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
     config.before_send = lambda do |event, _hint|
-      filter.filter(event.to_hash)
+      if event.extra
+        event.extra = filter.filter(event.extra)
+      end
+
+      if event.user
+        event.user = filter.filter(event.user)
+      end
+
+      if event.contexts
+        event.contexts = filter.filter(event.contexts)
+      end
+
+      event
     end
 
     config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
