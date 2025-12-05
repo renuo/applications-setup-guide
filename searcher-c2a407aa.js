a4309408ca260ee9032ddf3e49abf93470cee378
rnestler <rnestler@users.noreply.github.com>
2025-12-05 15:10:58 +0000

    deploy: c26931ee33cba25d60243be9bd95b8ddad05e88d

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7ea8ab80.js',
+                path_to_root + 'searchindex-d60be6af.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
