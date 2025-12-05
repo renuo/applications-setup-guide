a197501402a0e27a8a3b27d80eee8a1dd7d55684
rnestler <rnestler@users.noreply.github.com>
2025-12-05 14:54:03 +0000

    deploy: 86da13d5d3c787831820a75b7ffc23a1a2c3ded8

--- templates/config/initializers/sentry.rb
+++ templates/config/initializers/sentry.rb
@@ -6,7 +6,10 @@ if defined? Sentry
   Sentry.init do |config|
     filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
     config.before_send = lambda do |event, _hint|
-      filter.filter(event.to_hash)
+      event.extra = filter.filter(event.extra) if event.extra
+      event.user = filter.filter(event.user) if event.user    
+      event.contexts = filter.filter(event.contexts) if event.contexts
+      event
     end
 
     config.breadcrumbs_logger = %i[sentry_logger active_support_logger http_logger]
