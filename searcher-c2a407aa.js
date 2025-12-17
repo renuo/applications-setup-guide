e5ab63cba8b9e04221ba058d8ef78adebf01917f
schmijos <schmijos@users.noreply.github.com>
2025-12-17 09:20:44 +0000

    deploy: a5980032c0314fc35cea51010a0a05f9c050c917

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-06534f22.js',
+                path_to_root + 'searchindex-d596a157.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
