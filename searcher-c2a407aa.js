544578cfdfd025da7744dffd58306dd15c6d5eaf
coorasse <coorasse@users.noreply.github.com>
2026-01-20 09:16:39 +0000

    deploy: 3082445a048a5079a84aad5df975b029d8d81bb7

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7a4acf39.js',
+                path_to_root + 'searchindex-71e6b70d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
